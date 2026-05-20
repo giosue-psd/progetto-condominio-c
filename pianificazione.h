e26e3ad56251d9c823da8799c5cccd2bdb9fa542
Jacopo Salimbene <j.salimbene@studenti.unisa.it>
2026-05-20 09:31:28 +0200

    caricamento pianificazione.h

---
+++ pianificazione.h
@@ -0,0 +1,10 @@
+#ifndef PIANIFICAZIONE_H
+#define PIANIFICAZIONE_H
+
+#include "condominio_tipi.h"
+#include "richieste.h"
+#include "tecnici.h"
+
+bool pianificaIntervento(SistemaCondominio* sys, int id_richiesta, int id_tecnico, Data d, FasciaOraria fascia);
+
+#endif
