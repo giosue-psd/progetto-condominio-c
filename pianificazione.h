b13169aa4de1a1c7424f419bf3cb253a45feaf2c
jacopo salimbene <j.salimbene@studenti.unisa.it>
2026-05-26 18:21:25 +0200

    aggiunta commenti

--- pianificazione.h
+++ pianificazione.h
@@ -1,10 +1,11 @@
+/* scritto da Jacopo Salimbene */
 #ifndef PIANIFICAZIONE_H
 #define PIANIFICAZIONE_H
 
 #include "condominio_tipi.h"
 #include "richieste.h"
 #include "tecnici.h"
-
+/*prototipo */
 bool pianificaIntervento(SistemaCondominio* sys, int id_richiesta, int id_tecnico, Data d, FasciaOraria fascia);
 
 #endif
